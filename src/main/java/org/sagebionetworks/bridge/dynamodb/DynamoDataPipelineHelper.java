a0a2cac4198582ff4146e5a0dd2151ab98d21bb6
Joshua Liu <joshua.liu@sagebase.org>
2016-04-01 11:46:46 -0700

    PR feedback
    * Switch from java.time to joda time
    * Improve test coverage

--- src/main/java/org/sagebionetworks/bridge/dynamodb/DynamoDataPipelineHelper.java
+++ src/main/java/org/sagebionetworks/bridge/dynamodb/DynamoDataPipelineHelper.java
@@ -1,21 +1,16 @@
 package org.sagebionetworks.bridge.dynamodb;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
 import java.util.Collection;
 import java.util.List;
 
 import com.amazonaws.regions.Region;
 import com.amazonaws.services.datapipeline.model.Field;
 import com.amazonaws.services.datapipeline.model.PipelineObject;
-import com.amazonaws.services.dynamodbv2.model.TableDescription;
 import com.google.common.collect.Lists;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.LocalTime;
+import org.joda.time.format.ISODateTimeFormat;
 
 /**
  * Creates a Data Pipeline for backing up Dynamo DB tables within a region.
@@ -27,21 +22,21 @@ public class DynamoDataPipelineHelper {
     /**
      * Creates data pipeline objects for a daily data pipeline that will export data for DynamoDB tables in a region.
      *
-     * @param tables       dynamoDB tables to dump
-     * @param dynamoRegion region that the dynamoDB tables belong to
-     * @param s3Bucket     s3 bucket for dynamo table dump and logs, table dumps will go in
-     *                     s3://s3Bucket/regionName/tableName/, logs will go in s3://s3Bucket/logs
-     * @param localTime    time to begin runnin pipeline
+     * @param tableNames     dynamoDB tables to dump
+     * @param dynamoRegion   region that the dynamoDB tables belong to
+     * @param s3Bucket       s3 bucket for dynamo table dump and logs, table dumps will go in
+     *                       s3://s3Bucket/regionName/tableName/, logs will go in s3://s3Bucket/logs
+     * @param localStartTime time to begin running pipeline each day
      */
     public static List<PipelineObject> createPipelineObjects(Region dynamoRegion,
-                                                             Collection<TableDescription> tables,
+                                                             Collection<String> tableNames,
                                                              String s3Bucket,
-                                                             LocalTime localTime) {
+                                                             LocalTime localStartTime,
+                                                             DateTimeZone localDateTimeZone) {
         List<PipelineObject> pipelineObjects = Lists.newArrayList();
 
-        ZonedDateTime dateTime = ZonedDateTime.of(LocalDate.now(), localTime, ZoneId.systemDefault());
 
-        pipelineObjects.add(getRunDailySchedule(dateTime));
+        pipelineObjects.add(getRunDailySchedule(localStartTime, localDateTimeZone));
 
         pipelineObjects.add(getDefault(s3Bucket));
 
@@ -49,27 +44,27 @@ public class DynamoDataPipelineHelper {
 
         pipelineObjects.add(cluster);
 
-        for (TableDescription table : tables) {
-            pipelineObjects.addAll(getEMRActivity(table, cluster, dynamoRegion, s3Bucket));
+        for (String tableName : tableNames) {
+            pipelineObjects.addAll(getEMRActivity(tableName, cluster, dynamoRegion, s3Bucket));
         }
 
         return pipelineObjects;
     }
 
-    static PipelineObject getRunDailySchedule(ZonedDateTime startDateTime) {
-        LocalDateTime utcStartDateTime = startDateTime.withZoneSameInstant(ZoneOffset.UTC).toLocalDateTime();
-
+    static PipelineObject getRunDailySchedule(LocalTime localStartTime, DateTimeZone localTimeZone) {
         String name = "DailySchedule";
         String id = "Schedule";
-        String startDateTimeString =
-                utcStartDateTime.truncatedTo(ChronoUnit.SECONDS).format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+
+        DateTime dateTime = localStartTime.toDateTimeToday(localTimeZone).toDateTime(DateTimeZone.UTC);
+
+        // Drop to second-level precision, or Data Pipeline will reject
+        String startDateTimeString = dateTime.toString(ISODateTimeFormat.dateHourMinuteSecond());
 
         return new PipelineObject().withName(name).withId(id).withFields(
                 new Field().withKey("type").withStringValue("Schedule"),
                 new Field().withKey("startDateTime").withStringValue(startDateTimeString),
                 new Field().withKey("period").withStringValue("1 day")
         );
-
     }
 
     /**
@@ -123,11 +118,10 @@ public class DynamoDataPipelineHelper {
         return new PipelineObject().withName(name).withId(id).withFields(fieldsList);
     }
 
-    static List<PipelineObject> getEMRActivity(TableDescription table,
+    static List<PipelineObject> getEMRActivity(String tableName,
                                                PipelineObject assignedCluster,
                                                Region region,
                                                String bucket) {
-        String tableName = table.getTableName();
         String name = "TableBackupActivity-" + tableName;
         String id = "TableBackupActivity-" + tableName;
 
@@ -157,11 +151,13 @@ public class DynamoDataPipelineHelper {
         String name = "EmrClusterForBackup";
         String id = "EmrClusterForBackup";
 
+        //Discussion on scaling: https://forums.aws.amazon.com/thread.jspa?messageID=667827
+
         return new PipelineObject().withName(name).withId(id).withFields(
                 new Field().withKey("type").withStringValue("EmrCluster"),
                 new Field().withKey("amiVersion").withStringValue("3.8.0"),
-                new Field().withKey("masterInstanceType").withStringValue("m3.xlarge"),
-                new Field().withKey("coreInstanceType").withStringValue("m3.xlarge"),
+                new Field().withKey("masterInstanceType").withStringValue("m1.medium"),
+                new Field().withKey("coreInstanceType").withStringValue("m1.medium"),
                 new Field().withKey("coreInstanceCount").withStringValue("1"),
                 new Field().withKey("region").withStringValue(region.getName()),
                 new Field().withKey("terminateAfter").withStringValue("12 hours"),
