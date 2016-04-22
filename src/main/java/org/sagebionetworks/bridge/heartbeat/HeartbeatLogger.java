b41b3f7ac52f392e91275bef51191ba83051a28e
Dwayne Jeng <dwayne.jeng@sagebase.org>
2016-04-22 01:23:23 -0700

    implement heartbeat without jcabi, add tests

--- src/main/java/org/sagebionetworks/bridge/heartbeat/HeartbeatLogger.java
+++ src/main/java/org/sagebionetworks/bridge/heartbeat/HeartbeatLogger.java
@@ -1,24 +1,50 @@
 package org.sagebionetworks.bridge.heartbeat;
 
-import java.util.concurrent.TimeUnit;
-
-import com.jcabi.aspects.ScheduleWithFixedDelay;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Heartbeat logger. Used to write something to the logs every 30 minutes to ensure you have logs for every hour.
- * Generally useful for logging utilities that assume there will be a log message every hour. Uses Jcabi.
- *
- * 30 minutes chosen to ensure there's at least 1 log (generally 2), while 60 minutes might cause a log at 1:59 and
- * 3:01 if there's severe clock skew.
+ * Heartbeat logger. Used to write something to the logs on a regular interval to ensure you have logs for every hour.
+ * Generally useful for logging utilities that assume there will be a log message every hour.
  */
-@ScheduleWithFixedDelay(delay = 30, unit = TimeUnit.MINUTES)
 public class HeartbeatLogger implements Runnable {
     private static final Logger LOG = LoggerFactory.getLogger(HeartbeatLogger.class);
 
+    private int intervalMillis;
+
+    /** Sets the heartbeat interval in minutes. */
+    public final void setIntervalMinutes(int intervalMinutes) {
+        // convert this internally to milliseconds
+        this.intervalMillis = intervalMinutes * 60000;
+    }
+
+    /**
+     * Runs the heartbeat logger. This is an infinite loop that waits the interval and logs the heartbeat message.
+     * First heartbeat message happens immediately.
+     */
     @Override
     public void run() {
+        while (shouldKeepRunning()) {
+            logHeartbeat();
+
+            if (intervalMillis > 0) {
+                try {
+                    Thread.sleep(intervalMillis);
+                } catch (InterruptedException ex) {
+                    LOG.warn("Interrupted while sleeping: " + ex.getMessage(), ex);
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+
+    /** Helper method, used for tests to verify logging was called. */
+    void logHeartbeat() {
         LOG.info("Logging heartbeat...");
+    }
+
+    /** True if the heartbeat logger should keep running. Overridden by tests to allow tests to exit. */
+    boolean shouldKeepRunning() {
+        return true;
     }
 }
