3a4c656e0e7d85297f9e1438f0be4c00ea478028
zaclin <zhizhen.lin@sagebase.org>
2017-03-24 15:43:25 -0700

    change scan args

--- src/main/java/org/sagebionetworks/bridge/dynamodb/DynamoScanHelper.java
+++ src/main/java/org/sagebionetworks/bridge/dynamodb/DynamoScanHelper.java
@@ -1,13 +1,11 @@
 package org.sagebionetworks.bridge.dynamodb;
 
-import java.util.Map;
-
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.api.ScanApi;
 
 public class DynamoScanHelper {
     /** Scan a DDB object. */
-    public Iterable<Item> scan(ScanApi scannable, String filterExpression, String projectionExpression, Map<String, String> nameMap, Map<String, Object> valueMap) {
-        return scannable.scan(filterExpression, projectionExpression, nameMap, valueMap);
+    public Iterable<Item> scan(ScanApi scannable) {
+        return scannable.scan();
     }
 }
