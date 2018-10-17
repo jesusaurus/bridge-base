e08f337b43bd776349e11e416b7602f534037f56
Alx Dark <alx.dark@sagebase.org>
2018-10-17 13:55:21 -0700

    Dependency updates for BRIDGE-2334

--- src/main/java/org/sagebionetworks/bridge/config/PropertiesConfig.java
+++ src/main/java/org/sagebionetworks/bridge/config/PropertiesConfig.java
@@ -2,13 +2,11 @@ package org.sagebionetworks.bridge.config;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.Properties;
 import java.util.regex.Pattern;
