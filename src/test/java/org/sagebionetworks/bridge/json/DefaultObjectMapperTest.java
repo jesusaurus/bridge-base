138f95898e0821a18f080300716a9b8b2dc0c1aa
Alx Dark <alx.dark@sagebase.org>
2020-05-26 10:43:13 -0700

    Jackson should not throw an error when there are unknown JSON properties, it's too strict to migrate code.

---
+++ src/test/java/org/sagebionetworks/bridge/json/DefaultObjectMapperTest.java
@@ -0,0 +1,27 @@
+package org.sagebionetworks.bridge.json;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import org.testng.annotations.Test;
+
+public class DefaultObjectMapperTest {
+
+    private static class TestObject {
+        private String name;
+        private String value;
+        @JsonCreator
+        public TestObject(@JsonProperty("name") String name, 
+                @JsonProperty("value") String value) {
+            this.name = name;
+            this.value = value;
+        }
+    }
+    
+    @Test
+    public void silentlyIgnoresUnknownProperties() throws Exception {
+        String json = "{\"name\": \"aName\", \"value\": \"aValue\", \"third\": \"unknownProperty\"}";
+        TestObject object = DefaultObjectMapper.INSTANCE.readValue(json, TestObject.class);
+    }
+    
+}
